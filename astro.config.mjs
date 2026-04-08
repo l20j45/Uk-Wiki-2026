116266c587d5a2381ef4d6a37c62c27db95dd6bd
Daniel Rojas <l20j45_@gmail.com>
2026-04-08 11:21:04 -0600

    Revision Final

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,25 +7,66 @@ import sitemap from "@astrojs/sitemap";
 
 import netlify from "@astrojs/netlify";
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import AstroPWA from '@vite-pwa/astro';
 
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
 
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), AstroPWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Inglaterra Wiki',
+        short_name: 'UK Wiki',
+        description: 'Itinerario y guía de viaje a Inglaterra',
+        theme_color: '#4f46e5', // El índigo de tus botones
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        // Cacheamos todas las rutas que empiecen por /articles o /itinerary
+        globPatterns: ['**/*.{js,css,html,svg,png,jpg}'],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.startsWith('/'),
+            handler: 'NetworkFirst', // Intenta red, si falla usa caché (ideal para tu Wiki)
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 7, // 1 semana
+              },
+            },
+          },
+        ],
+      },
+    }),],
   site: "https://uk2026gdl.netlify.app/",
-  server: {
-    // Esto es para que Astro escuche en todas las interfaces de red
-    host: true, 
-    port:4321,
-  },
+  // server: {
+  //   // Esto es para que Astro escuche en todas las interfaces de red
+  //   host: true, 
+  //   port:4321,
+  // },
   vite: {
-    server: {
-      allowedHosts: 
-      true
-      ,
-    },
+    // server: {
+    //   allowedHosts: 
+    //   true
+    //   ,
+    // },
     build: {
       // Optimización de chunks para que el JS de React no pese tanto
       rollupOptions: {
@@ -38,7 +79,8 @@ export default defineConfig({
         },
       },
     },
-    plugins: [tailwindcss(), basicSsl()],
+    // plugins: [tailwindcss(), basicSsl()],
+    plugins: [tailwindcss()],
     ssr: {
       external: ["cloudinary"],
     },
